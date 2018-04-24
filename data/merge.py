9148a2751729f415ee2fbe9e706c0c0658e080ce
Khuyen Cao <hakhuyen1997@gmail.com>
2018-04-24 01:01:55 -0400

    merge data

---
+++ data/merge.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df1 = pd.read_csv('data-scientist.csv')
+df2 = pd.read_csv('software-engineer.csv')
+df3 = pd.read_csv('computer-systems.csv')
+
+frames = [df1, df2, df3]
+result = pd.concat(frames)
+result.drop_duplicates()
+result.drop(result.columns[0], axis=1)
+
+result.to_csv('out.csv')
